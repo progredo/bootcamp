a38f79e8dee2763ba83c1416c6a6f924ccf6367b
rob salmond <rob@salmond.ca>
2024-05-16 11:29:00 -0400

    adding basic aws structure

---
+++ aws/dev/tofu.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.7.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.49.0"
+    }
+  }
+  backend "gcs" {
+    bucket = "bootcamp-infra-tf-state"
+    prefix = "progredo/bootcamp/aws/dev"
+  }
+}
+
+provider "aws" {
+  profile = "bc-dev"
+  region = "us-east-1"
+}
---
+++ aws/prod/tofu.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.7.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.49.0"
+    }
+  }
+  backend "gcs" {
+    bucket = "bootcamp-infra-tf-state"
+    prefix = "progredo/bootcamp/aws/prod"
+  }
+}
+
+provider "aws" {
+  profile = "bc-prod"
+  region = "us-east-1"
+}
